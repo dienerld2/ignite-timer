c628e21d434916a227cbacb60b4638e1c482982a
Diener Dornelas <dienezim@hotmail.com>
2023-01-08 08:39:14 -0300

    refactor if to switch

--- src/app/Contexts/CyclesContext.tsx
+++ src/app/Contexts/CyclesContext.tsx
@@ -46,41 +46,44 @@ export function CyclesContextProvider({
 }: CyclesContextProviderProps) {
   const [cyclesState, dispatch] = useReducer(
     (state: CyclesState, action: any) => {
-      if (action.type === ActionReducer.ADD_NEW_CYCLE) {
-        return {
-          ...state,
-          cycles: [...state.cycles, action.payload.newCycle],
-          activeCycleId: action.payload.newCycle.id,
-        };
-      }
+      switch (action.type) {
+        case ActionReducer.ADD_NEW_CYCLE: {
+          return {
+            ...state,
+            cycles: [...state.cycles, action.payload.newCycle],
+            activeCycleId: action.payload.newCycle.id,
+          };
+        }
 
-      if (action.type === ActionReducer.INTERRUPT_CURRENT_CYCLE) {
-        return {
-          ...state,
-          cycles: state.cycles.map((cycle) => {
-            if (cycle.id === state.activeCycleId) {
-              return { ...cycle, interruptedDate: new Date() };
-            }
-            return cycle;
-          }),
-          activeCycleId: null,
-        };
-      }
+        case ActionReducer.INTERRUPT_CURRENT_CYCLE: {
+          return {
+            ...state,
+            cycles: state.cycles.map((cycle) => {
+              if (cycle.id === state.activeCycleId) {
+                return { ...cycle, interruptedDate: new Date() };
+              }
+              return cycle;
+            }),
+            activeCycleId: null,
+          };
+        }
 
-      if (action.type === ActionReducer.MARK_CURRENT_CYCLE_AS_FINISHED) {
-        return {
-          ...state,
-          cycles: state.cycles.map((cycle) => {
-            if (cycle.id === activeCycleId) {
-              return { ...cycle, finishedDate: new Date() };
-            }
-            return cycle;
-          }),
-          activeCycleId: null,
-        };
-      }
+        case ActionReducer.MARK_CURRENT_CYCLE_AS_FINISHED: {
+          return {
+            ...state,
+            cycles: state.cycles.map((cycle) => {
+              if (cycle.id === activeCycleId) {
+                return { ...cycle, finishedDate: new Date() };
+              }
+              return cycle;
+            }),
+            activeCycleId: null,
+          };
+        }
 
-      return state;
+        default:
+          return state;
+      }
     },
     {
       cycles: [],
